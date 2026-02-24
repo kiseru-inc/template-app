b49d5e4e8dd9fe7a49596a7ad70a976a5476ed92
Yuki Ando <yukando@kiseru.jp>
2026-02-24 18:11:18 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,29 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
+import { APP } from "../config/app";
+
+export const metadata: Metadata = {
+  title: APP.name,
+  description: APP.description,
+};
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html>
-      <body>{children}</body>
+    <html lang="ja">
+      <body>
+        <Header />
+
+        <main className="container">{children}</main>
+
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
